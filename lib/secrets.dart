fe719a0b95745e3050d2ce170210113e22e3e9f6
mahesh jamdade <maheshmn121@gmail.com>
2021-06-05 20:10:07 +0530

    add secrets

---
+++ lib/secrets.dart
@@ -0,0 +1,5 @@
+const String JWTSecret = "7dbe40bd-0d1d-4e3a-9d1d-ed0f1b44acfe";
+const String APIkey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMjcyMzQ1NywiZXhwIjoxOTM4Mjk5NDU3fQ._E91QdE0oJVDTPfvZgwADOSn-eb7dhniPPX_OTpQMWo";
+const String ServiceRole =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjIyNzIzNDU3LCJleHAiOjE5MzgyOTk0NTd9.lLLsM6fxK2t59qdSFJQtOrH5ttRMbJ44whZFJNbURlk";
