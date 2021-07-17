9fd52d42d82d19de8f1232550449bad7a08559cf
mahesh jamdade <maheshmn121@gmail.com>
2021-07-17 16:11:39 +0530

    remove come

--- lib/pages/splashscreen.dart
+++ lib/pages/splashscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vocabhub/exports.dart';
+import 'package:vocabhub/main.dart';
 import 'package:vocabhub/pages/home.dart';
 import 'package:vocabhub/utils/navigator.dart';
 
