3214bc6467d964c298f9851d3a31ccd3e28911a3
mahesh jamdade <maheshmn121@gmail.com>
2021-07-17 15:11:34 +0530

    theme issue with appbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:vocabhub/constants/constants.dart';
 import 'package:vocabhub/main.dart';
 import 'package:vocabhub/models/word_model.dart';
@@ -109,45 +110,41 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: DrawerBuilder(),
               )
             : null,
-        appBar: constraints.maxWidth < MOBILE_WIDTH
-            ? null
-            : AppBar(
-                backgroundColor: isDark ? null : Colors.white,
-                iconTheme: Theme.of(context).iconTheme,
-                centerTitle:
-                    constraints.maxWidth <= MOBILE_WIDTH ? true : false,
-                title: Text(
-                  '$APP_TITLE',
-                  style: TextStyle(color: isDark ? Colors.white : primaryColor),
-                ),
-                actions: [
-                  actionIcon(Icons.add, '', toolTip: 'Add a word', onTap: () {
-                    // _openCustomDialog();
-                    navigate(context, AddWordForm(),
-                        slideTransitionType: SlideTransitionType.btt);
-                  }),
-                  // constraints.maxWidth <= MOBILE_WIDTH
-                  //     ? Container()
-                  //     : IconButton(
-                  //         icon: Image.asset(
-                  //           !isDark
-                  //               ? '$GITHUB_ASSET_PATH'
-                  //               : '$GITHUB_WHITE_ASSET_PATH',
-                  //           height: isDark ? 26 : 35,
-                  //         ),
-                  //         tooltip: 'Github',
-                  //         padding: EdgeInsets.zero,
-                  //         onPressed: () {
-                  //           launchUrl(SOURCE_CODE_URL, isNewTab: true);
-                  //         },
-                  //       ),
-                  actionIcon(Icons.code, SOURCE_CODE_URL,
-                      toolTip: 'Source code'),
-                  actionIcon(Icons.privacy_tip, PRIVACY_POLICY,
-                      toolTip: 'Privacy Policy'),
-                  actionIcon(Icons.bug_report, REPORT_URL, toolTip: 'Report'),
-                ],
-              ),
+        appBar: AppBar(
+          backgroundColor: isDark ? null : Colors.white,
+          iconTheme: Theme.of(context).iconTheme,
+          centerTitle: constraints.maxWidth <= MOBILE_WIDTH ? true : false,
+          title: Text(
+            '$APP_TITLE',
+            style: TextStyle(color: isDark ? Colors.white : primaryColor),
+          ),
+          actions: [
+            actionIcon(Icons.add, '', toolTip: 'Add a word', onTap: () {
+              // _openCustomDialog();
+              navigate(context, AddWordForm(),
+                  slideTransitionType: SlideTransitionType.btt);
+            }),
+            // constraints.maxWidth <= MOBILE_WIDTH
+            //     ? Container()
+            //     : IconButton(
+            //         icon: Image.asset(
+            //           !isDark
+            //               ? '$GITHUB_ASSET_PATH'
+            //               : '$GITHUB_WHITE_ASSET_PATH',
+            //           height: isDark ? 26 : 35,
+            //         ),
+            //         tooltip: 'Github',
+            //         padding: EdgeInsets.zero,
+            //         onPressed: () {
+            //           launchUrl(SOURCE_CODE_URL, isNewTab: true);
+            //         },
+            //       ),
+            actionIcon(Icons.code, SOURCE_CODE_URL, toolTip: 'Source code'),
+            actionIcon(Icons.privacy_tip, PRIVACY_POLICY,
+                toolTip: 'Privacy Policy'),
+            actionIcon(Icons.bug_report, REPORT_URL, toolTip: 'Report'),
+          ],
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             darkNotifier.value = !darkNotifier.value;
@@ -246,6 +243,28 @@ class _WordsBuilderState extends State<WordsBuilder> {
     return result;
   }
 
+  _onEndScroll(ScrollMetrics metrics) {}
+
+  _onUpdateScroll(ScrollMetrics metrics) {}
+
+  _onStartScroll(ScrollMetrics metrics) {
+    if (_scrollController.position.userScrollDirection ==
+        ScrollDirection.reverse) {
+      setState(() {
+        topPosition = -80.0;
+      });
+    } else {
+      setState(() {
+        topPosition = topPadding;
+      });
+    }
+  }
+
+  final ScrollController _scrollController = ScrollController();
+  bool upDirection = true, flag = true;
+
+  double topPadding = 48.0;
+  double topPosition = 48.0;
   RefreshController _refreshController =
       RefreshController(initialRefresh: false);
   List<Word> supaStoreWords = [];
@@ -253,6 +272,8 @@ class _WordsBuilderState extends State<WordsBuilder> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    topPadding = MediaQuery.of(context).padding.top;
+
     return ValueListenableBuilder<List<Word>?>(
         valueListenable: listNotifier,
         builder: (BuildContext context, List<Word>? value, Widget? child) {
@@ -263,38 +284,31 @@ class _WordsBuilderState extends State<WordsBuilder> {
           }
           return Container(
             color: darkNotifier.value ? primaryDark : Colors.white,
-            child: Column(
+            child: Stack(
               children: [
-                SearchBuilder(
-                  onChanged: (x) {
-                    if (x.isEmpty) {
-                      listNotifier.value = supaStoreWords;
-                      return;
-                    }
-                    List<Word> result = [];
-                    supaStoreWords.forEach((element) {
-                      if (element.word
-                              .toLowerCase()
-                              .contains(x.toLowerCase()) ||
-                          element.meaning
-                              .toLowerCase()
-                              .contains(x.toLowerCase()) ||
-                          isInSynonym(x, element.synonyms)) {
-                        result.add(element);
+                SmartRefresher(
+                  enablePullDown: size.width > MOBILE_WIDTH ? false : true,
+                  controller: _refreshController,
+                  onRefresh: () => refresh(),
+                  child: NotificationListener<ScrollNotification>(
+                    onNotification: (scrollNotification) {
+                      if (scrollNotification is ScrollStartNotification) {
+                        _onStartScroll(scrollNotification.metrics);
+                        return false;
+                      } else if (scrollNotification
+                          is ScrollUpdateNotification) {
+                        _onUpdateScroll(scrollNotification.metrics);
+                        return true;
+                      } else if (scrollNotification is ScrollEndNotification) {
+                        _onEndScroll(scrollNotification.metrics);
+                        return true;
                       }
-                    });
-                    listNotifier.value = result;
-                  },
-                ),
-                Expanded(
-                  // TODO: Wrap with scrollbar widget currently it is buggy
-                  child: SmartRefresher(
-                    enablePullDown: size.width > MOBILE_WIDTH ? false : true,
-                    enablePullUp: false,
-                    controller: _refreshController,
-                    onRefresh: () => refresh(),
+                      return false;
+                    },
                     child: ListView.builder(
+                        padding: EdgeInsets.only(top: topPadding + 60),
                         itemCount: value.length,
+                        controller: _scrollController,
                         itemBuilder: (_, x) {
                           return WordTile(
                               word: value[x],
@@ -308,6 +322,34 @@ class _WordsBuilderState extends State<WordsBuilder> {
                                 widget.onSelect!(word);
                               });
                         }),
+                  ),
+                ),
+                AnimatedPositioned(
+                  top: topPosition,
+                  duration: Duration(milliseconds: 400),
+                  child: Container(
+                    width: size.width,
+                    child: SearchBuilder(
+                      onChanged: (x) {
+                        if (x.isEmpty) {
+                          listNotifier.value = supaStoreWords;
+                          return;
+                        }
+                        List<Word> result = [];
+                        supaStoreWords.forEach((element) {
+                          if (element.word
+                                  .toLowerCase()
+                                  .contains(x.toLowerCase()) ||
+                              element.meaning
+                                  .toLowerCase()
+                                  .contains(x.toLowerCase()) ||
+                              isInSynonym(x, element.synonyms)) {
+                            result.add(element);
+                          }
+                        });
+                        listNotifier.value = result;
+                      },
+                    ),
                   ),
                 ),
               ],
