70cd60b33d81100efae9858c3761b6240ef31c41
mahesh jamdade <maheshmn121@gmail.com>
2021-08-01 00:30:28 +0530

    update version

--- lib/constants/const.dart
+++ lib/constants/const.dart
@@ -1,15 +1,16 @@
 /// APP CONSTANTS GO HERE IN THIS FILE
 
 const BASE_URL = '';
-const VERSION = 'v0.3.0';
+const VERSION = 'v0.3.1';
 const CONFIG_URL = 'https://bisasplfdnyiiggonpcx.supabase.co';
 const SOURCE_CODE_URL = 'https://github.com/maheshmnj/vocabhub';
-const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.vocabhub.app';
+const PLAY_STORE_URL =
+    'https://play.google.com/store/apps/details?id=com.vocabhub.app';
 const REPORT_URL = 'https://github.com/maheshmnj/vocabhub/issues/new/choose';
 // const SHEET_URL =
 //     'https://docs.google.com/spreadsheets/d/1G1RtQfsEDqHhHP4cgOpO9x_ZtQ1dYa6QrGCq3KFlu50';
 const PRIVACY_POLICY = 'https://maheshmnj.github.io/privacy';
-const MOBILE_WIDTH = 500.0;
+const MOBILE_WIDTH = 600.0;
 const TABLET_WIDTH = 800.0;
 const Duration wordCountAnimationDuration = Duration(seconds: 3);
 const TABLE_NAME = 'vocabsheet';
