ed1396c9f972fbac20911452d012b28bfdd6afae
mahesh jamdade <maheshmn121@gmail.com>
2023-06-18 11:13:04 -0400

    minor fix

--- lib/navbar/dashboard/dashboard.dart
+++ lib/navbar/dashboard/dashboard.dart
@@ -4,7 +4,6 @@ import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:navbar_router/navbar_router.dart';
 import 'package:vocabhub/exports.dart';
-import 'package:vocabhub/main.dart';
 import 'package:vocabhub/models/word.dart';
 import 'package:vocabhub/navbar/dashboard/bookmarks.dart';
 import 'package:vocabhub/pages/login.dart';
@@ -120,7 +119,6 @@ class DashboardMobile extends StatelessWidget {
               user!.isLoggedIn
                   ? IconButton(
                       onPressed: () {
-                        pushNotificationService.showFlutterNotification();
                         Navigate.pushNamed(context, Notifications.route, isRootNavigator: true);
                       },
                       icon: Icon(
