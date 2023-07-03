c8dfbfe80156b5c69281bf55a28b2b6998366c61
mahesh jamdade <maheshmn121@gmail.com>
2023-07-02 20:58:08 -0400

    undo patch

--- lib/navbar/dashboard/dashboard.dart
+++ lib/navbar/dashboard/dashboard.dart
@@ -189,7 +189,7 @@ class DashboardMobile extends StatelessWidget {
               children: [
                 Padding(
                   padding: 16.0.verticalPadding,
-                  child: heading('Word of the day(Patched)'),
+                  child: heading('Word of the day'),
                 ),
                 OpenContainer<bool>(
                     openBuilder: (BuildContext context, VoidCallback openContainer) {
