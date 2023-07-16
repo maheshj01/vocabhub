8fd29df2899f6097c0427a7ceaa0e903fd2b476e
mahesh jamdade <maheshmn121@gmail.com>
2023-07-16 13:38:55 -0400

    add signin Test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,24 +1,85 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:vocabhub/main.dart' as app;
 import 'package:vocabhub/navbar/navbar.dart';
+import 'package:vocabhub/pages/login.dart';
+
+extension FindText on String {
+  Finder textX() => find.text(this);
+}
+
+extension FindKey on Key {
+  Finder keyX() => find.byKey(this);
+}
+
+extension FindType on Type {
+  Finder typeX() => find.byType(this);
+}
+
+extension FindWidget on Widget {
+  Finder widgetX() => find.byWidget(this);
+}
+
+extension FindIcon on IconData {
+  Finder iconX() => find.byIcon(this);
+}
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('Test App', () {
-    testWidgets('Test SplashScreen should load', (WidgetTester tester) async {
+  group('Test App should load', () {
+    testWidgets('User should be able to login', (WidgetTester tester) async {
       // runZonedGuarded(app.main, (error, stack) {
       //   print('runZonedGuarded: Caught error in my root zone.$error $stack');
       // });
       app.main();
       await Future.delayed(const Duration(seconds: 3));
       await tester.pumpAndSettle();
+      expect((app.VocabApp).typeX(), findsOneWidget);
+      await tester.pumpAndSettle();
+      expect((Dashboard).typeX(), findsOneWidget);
+      await tester.pumpAndSettle();
+      await tester.pumpAndSettle();
 
-      expect(find.byType(app.VocabApp), findsOneWidget);
+      final dashboardIcon = Icons.dashboard.iconX();
+      final usericon = Icons.person.iconX();
+
+      expect(dashboardIcon, findsOneWidget);
+      expect(usericon, findsNothing);
+      final signIntextFinder = "Sign In".textX();
+      expect(signIntextFinder, findsOneWidget);
       await tester.pumpAndSettle();
-      expect(find.byType(Dashboard), findsOneWidget);
+      await tester.tap(signIntextFinder);
       await tester.pumpAndSettle();
+
+      // Sign In flow
+
+      expect((AppSignIn).typeX(), findsOneWidget);
+      await tester.pumpAndSettle();
+      expect((Dashboard).typeX(), findsNothing);
+      await tester.pumpAndSettle();
+
+      final signInTextFinder = "Sign In with Google".textX();
+      expect(signInTextFinder, findsOneWidget);
+      await tester.pumpAndSettle();
+      await tester.tap(signInTextFinder);
+      await tester.pumpAndSettle();
+      expect((Dashboard).typeX(), findsOneWidget);
+      await tester.pumpAndSettle();
+      Future.delayed(const Duration(seconds: 3));
+      await tester.pumpAndSettle();
+      expect(dashboardIcon, findsOneWidget);
+      expect(usericon, findsOneWidget);
     });
+
+    // testWidgets("User stays loggedIn", (widgetTester) async {
+    //   expect((Dashboard).typeX(), findsOneWidget);
+    //   await widgetTester.pumpAndSettle();
+    //   final dashboardIcon = Icons.dashboard.iconX();
+    //   final usericon = Icons.person.iconX();
+    //   expect(dashboardIcon, findsOneWidget);
+    //   expect(usericon, findsOneWidget);
+    // });
   });
 }
