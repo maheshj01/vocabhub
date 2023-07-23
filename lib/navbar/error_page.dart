e2449e01c1a13cf55a8021d6cbe76029603f6363
mahesh jamdade <maheshmn121@gmail.com>
2023-07-23 01:00:54 -0400

    add error animation

--- lib/navbar/error_page.dart
+++ lib/navbar/error_page.dart
@@ -1,21 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+import 'package:vocabhub/utils/utils.dart';
 
-class ErrorPage extends StatelessWidget {
+class ErrorPage extends StatefulWidget {
   final VoidCallback onRetry;
   final String errorMessage;
   const ErrorPage({Key? key, required this.onRetry, required this.errorMessage}) : super(key: key);
 
   @override
+  State<ErrorPage> createState() => _ErrorPageState();
+}
+
+class _ErrorPageState extends State<ErrorPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  RiveAnimationController _controller = OneShotAnimation('Animation 1');
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onRetry,
+      onTap: widget.onRetry,
       child: Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          SizedBox(
+            height: SizeUtils.size.width * 0.8,
+            child: RiveAnimation.asset(
+              'assets/rive/error.riv',
+              fit: BoxFit.cover,
+              controllers: [_controller],
+            ),
+          ),
+          16.0.vSpacer(),
           Text(
-            errorMessage,
+            widget.errorMessage,
             textAlign: TextAlign.center,
             style: Theme.of(context).textTheme.titleLarge,
           ),
@@ -32,6 +60,3 @@ class ErrorPage extends StatelessWidget {
     );
   }
 }
-
-
-
